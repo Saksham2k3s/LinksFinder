b22e0677eb93aa67b2423a853e46afe5ccdf74cf
Saksham2k3s <sakshamshrivastav58@gmail.com>
2023-11-18 14:15:00 +0530

    added para at homepage

--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -22,6 +22,12 @@ function HomePage() {
           centralized hub where you can effortlessly discover the most valuable
           and up-to-date video tutorials across the digital landscape.
         </p>
+        <p className={`text-center mt-5 ${mode === false ? 'darkText': 'lightText'}`} >
+          An innovative platform designed to simplify the process of finding
+          high-quality learning resources. FindTheBest aims to provide a
+          centralized hub where you can effortlessly discover the most valuable
+          and up-to-date video tutorials across the digital landscape.
+        </p>
          {/* <div className="d-flex justify-content-around mt-5">
           <button>Frontend</button>
           <button>Backend</button>
